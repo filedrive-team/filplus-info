f78c0856e26cf176e899c9ead0dd7077a31ae4e9
eric <qianhuanhuan@ipfsforce.com>
2021-07-30 12:00:04 +0800

    Update the second batch of notaries

--- models/notary.go
+++ models/notary.go
@@ -53,7 +53,6 @@ func GetNotaryList(offset, size int) (total int, list []*Notary, err error) {
 func NotaryList(params *types.PaginationParams) (*types.CommonList, error) {
 	type NotaryItem struct {
 		Notary
-		TxnId          int64           `json:"txn_id"`
 		Allowance      decimal.Decimal `json:"allowance"`
 		GrantAllowance decimal.Decimal `json:"grant_allowance"`
 	}
@@ -63,51 +62,36 @@ func NotaryList(params *types.PaginationParams) (*types.CommonList, error) {
 	stmtCount := `
 select 
 count(*)
-from notary n
--- quotas held by notary
-left join (
-select
-na.address,
-na.allowance,
-na.txn_id
 from (
-select na.address, max(txn_id) as txn_id 
-from notary_allowance na group by na.address
-) t2 left join notary_allowance na
-on t2.txn_id=na.txn_id
-) na 
-on n.address = na.address 
--- notary has issued quotas
-left join (
-select 
-ca.notary,
-sum(ca.allowance) as grant_allowance 
-from client_allowance ca 
-group by ca.notary 
-) t 
-on n.address = t.notary
+select
+n.notary_name
+from notary n
 where n.deleted_at is null
+group by n.notary_name
+) t
 `
 	stmt := `
-select 
-n.*,
-COALESCE(na.allowance,0) as allowance,
-COALESCE(na.txn_id,0) as txn_id,
-COALESCE(t.grant_allowance,0) as grant_allowance
+select
+max(n.id) as id,
+n.notary_name, 
+max(created_at) as created_at,
+max(updated_at) as updated_at,
+group_concat(distinct organization) as organization,
+group_concat(distinct location) as location,
+group_concat(distinct website) as website,
+group_concat(distinct github_user) as github_user,
+group_concat(n.address) as address,
+sum(COALESCE(na.allowance,0)) as allowance,
+sum(COALESCE(t.grant_allowance,0)) as grant_allowance
 from notary n
 -- quotas held by notary
 left join (
-select
-na.address,
-na.allowance,
-na.txn_id
-from (
-select na.address, max(txn_id) as txn_id 
-from notary_allowance na group by na.address
-) t2 left join notary_allowance na
-on t2.txn_id=na.txn_id
-) na 
-on n.address = na.address 
+select 
+address,
+sum(allowance) as allowance
+from notary_allowance
+group by address
+) na on n.address = na.address
 -- notary has issued quotas
 left join (
 select 
@@ -115,10 +99,10 @@ ca.notary,
 sum(ca.allowance) as grant_allowance 
 from client_allowance ca 
 group by ca.notary 
-) t 
-on n.address = t.notary
+) t on n.address = t.notary
 where n.deleted_at is null
-order by grant_allowance desc
+group by n.notary_name 
+order by location,id
 limit ? offset ?
 `
 	total := 0
@@ -165,6 +149,28 @@ func InitNotaryList() error {
 	}
 	list := []*Notary{
 		{
+			NotaryName:   "Philipp Banhardt",
+			Location:     "Europe",
+			Organization: "Filecoin Foundation",
+			Address:      "f1sdzgaqmitbvgktkklpuaxohg6nuhce5eyvwxhaa", // datacap had been reset，This record needs to be soft deleted
+			Website:      "@philipp on the Filecoin Slack",
+		},
+		{
+			NotaryName:   "Fleek",
+			Location:     "NA",
+			Organization: "Fleek",
+			Address:      "f2lda563hy3q2oxnoo265elynct3u2ft27ae7ptfy", // This record needs to be soft deleted
+			Website:      "fleek.co",
+		},
+
+		{
+			NotaryName:   "Masaaki Nawatani",
+			Location:     "Asia-GCN",
+			Organization: "Blotocol Japan Ltd.",
+			Address:      "f1fh53sdaie3yi25qxwcqxpt5h4naex5ibdaffibi",
+			Website:      "https://blotocol.com/",
+		},
+		{
 			NotaryName:   "MathWallet",
 			Location:     "Asia-GCN",
 			Organization: "MathWallet",
@@ -172,11 +178,23 @@ func InitNotaryList() error {
 			Website:      "mathwallet.org",
 		},
 		{
-			NotaryName:   "Masaaki Nawatani",
+			NotaryName:   "MathWallet",
 			Location:     "Asia-GCN",
-			Organization: "Blotocol Japan",
-			Address:      "f1fh53sdaie3yi25qxwcqxpt5h4naex5ibdaffibi",
-			Website:      "https://blotocol.com/",
+			Organization: "MathWallet",
+			Address:      "f136bz32l2hlyad4npmfg5dngcdw3jk3tpcpoz2ja",
+			Website:      "mathwallet.org",
+		},
+		{
+			NotaryName:   "12ships Foundation",
+			Location:     "Asia-GCN",
+			Organization: "N.A.",
+			Address:      "f1inc6lx4oosssdf5n7rkt45rtwzlip7ohott7vha",
+		},
+		{
+			NotaryName:   "Wijnand Schouten",
+			Location:     "Europe",
+			Organization: "Speedium",
+			Address:      "f1krmypm4uoxxf3g7okrwtrahlmpcph3y7rbqqgfa",
 		},
 		{
 			NotaryName:   "Keyko",
@@ -186,11 +204,10 @@ func InitNotaryList() error {
 			Website:      "https://www.keyko.io",
 		},
 		{
-			NotaryName:   "Julien NOEL",
+			NotaryName:   "Nicklas Reiersen",
 			Location:     "Europe",
-			Organization: "Julien NOEL",
-			Address:      "f1wxhnytjmklj2czezaqcfl7eb4nkgmaxysnegwii",
-			Website:      "s0nik42 on Filecoin Slack",
+			Organization: "TechHedge",
+			Address:      "f1oz43ckvmtxmmsfzqm6bpnemqlavz4ifyl524chq",
 		},
 		{
 			NotaryName:   "Philipp Banhardt",
@@ -200,39 +217,100 @@ func InitNotaryList() error {
 			Website:      "https://fil.org/",
 		},
 		{
-			NotaryName:   "Philipp Banhardt",
+			NotaryName:   "Megan Klimen",
 			Location:     "Europe",
 			Organization: "Filecoin Foundation",
-			Address:      "f1sdzgaqmitbvgktkklpuaxohg6nuhce5eyvwxhaa", // datacap已重置，该记录需软删除
-			Website:      "@philipp on the Filecoin Slack",
+			Address:      "f1k6wwevxvp466ybil7y2scqlhtnrz5atjkkyvm4a",
+		},
+		{
+			NotaryName:   "Julien NOEL",
+			Location:     "Europe",
+			Organization: "Twinquasar",
+			Address:      "f1wxhnytjmklj2czezaqcfl7eb4nkgmaxysnegwii",
+			Website:      "s0nik42 on Filecoin Slack",
 		},
 		{
 			NotaryName:   "Steven Li",
-			Location:     "GCN",
+			Location:     "GCR",
 			Organization: "IPFS Force",
 			Address:      "f1qoxqy3npwcvoqy7gpstm65lejcy7pkd3hqqekna",
 			Website:      "@Steven on Filecoin Slack, @Steven004_Li on Twitter",
 		},
 		{
 			NotaryName:   "Simon686",
-			Location:     "GCN",
+			Location:     "GCR",
 			Organization: "1475",
 			Address:      "f1lwpw2bcv66pla3lpkcuzquw37pbx7ur4m6zvq2a",
 			Website:      "1475ipfs.com, @simon686-1475 on Filecoin Slack",
 		},
 		{
+			NotaryName:   "Neo Ge",
+			Location:     "GCR",
+			Organization: "IPFSMain",
+			Address:      "f13k5zr6ovc2gjmg3lvd43ladbydhovpylcvbflpa",
+			Website:      "@Neo Ge on Filecoin Slack",
+		},
+		{
 			NotaryName:   "Fenbushi Capital",
-			Location:     "GCN",
+			Location:     "GCR",
 			Organization: "Fenbushi Capital",
 			Address:      "f1yqydpmqb5en262jpottko2kd65msajax7fi4rmq",
 			Website:      "https://fenbushi.vc",
 		},
 		{
-			NotaryName:   "Neo Ge",
-			Location:     "GCN",
-			Organization: "IPFSMain",
-			Address:      "f13k5zr6ovc2gjmg3lvd43ladbydhovpylcvbflpa",
-			Website:      "@Neo Ge on Filecoin Slack",
+			NotaryName:   "Wise R&D Center - waynewyang",
+			Location:     "GCR",
+			Organization: "Wise R&D Center",
+			Address:      "f1hx5mnvaajaf47ftcjnokbbat3x7gswnk3wempky",
+		},
+		{
+			NotaryName:   "ByteBase",
+			Location:     "GCR",
+			Organization: "Shanghai ByteBase Information Technology Co., Ltd",
+			Address:      "f1cywlxarzudoubnbcawdbitrqkryxoyagxrsliiq",
+		},
+		{
+			NotaryName:   "Binghe Distributed Storage Lab",
+			Location:     "GCR",
+			Organization: "Binghe Distributed Storage Lab",
+			Address:      "f14gme3f52prtyzk6pblogrdd6b6ivp4swc6qmesi",
+		},
+		{
+			NotaryName:   "Steve Song",
+			Location:     "GCR",
+			Organization: "IPFSUnion",
+			Address:      "f1enfilmuyphmnqexjt33zfbk56c25mo2lplgbpxa",
+		},
+		{
+			NotaryName:   "XnMatrix",
+			Location:     "NA",
+			Organization: "XnMatrix",
+			Address:      "f1yuz2twsllparyfqwslfiuxrc5wj4mfiflvnsw6a",
+		},
+		{
+			NotaryName:   "Emma Russell",
+			Location:     "NA",
+			Organization: "Koda Inc.",
+			Address:      "f1fb27mp6xwlfgcm4vhxwdrjx5ldsqtdtzu3vh6ay",
+		},
+		{
+			NotaryName:   "Darnell Washington",
+			Location:     "NA",
+			Organization: "SECUREXPERTS Inc.",
+			Address:      "f1bjdcjxha3ldcstw5zmvkyu3r2p5x2bsm745kgsi",
+		},
+		{
+			NotaryName:   "Performive",
+			Location:     "NA",
+			Organization: "Performive",
+			Address:      "f13vzzb65gr7pjmb2vsfq666epq6lhdbanc4vfopq",
+			Website:      "https://performive.com/",
+		},
+		{
+			NotaryName:   "Filswan",
+			Location:     "NA",
+			Organization: "Filswan - NBFS",
+			Address:      "f1hlubjsdkv4wmsdadihloxgwrz3j3ernf6i3cbpy",
 		},
 		{
 			NotaryName:   "Jonathan Schwartz",
@@ -243,13 +321,6 @@ func InitNotaryList() error {
 			Remark:       "Glif Verifier",
 		},
 		{
-			NotaryName:   "Jason Brozena",
-			Location:     "NA",
-			Organization: "Performive",
-			Address:      "f13vzzb65gr7pjmb2vsfq666epq6lhdbanc4vfopq",
-			Website:      "https://performive.com/",
-		},
-		{
 			NotaryName:   "Andrew Hill",
 			Location:     "NA",
 			Organization: "Textile",
@@ -257,23 +328,16 @@ func InitNotaryList() error {
 			Website:      "textile.io",
 		},
 		{
-			NotaryName:   "Fleek",
-			Location:     "NA",
-			Organization: "Fleek",
-			Address:      "f2lda563hy3q2oxnoo265elynct3u2ft27ae7ptfy",
-			Website:      "fleek.co",
-		},
-		{
-			NotaryName:   "Ann Shin",
-			Location:     "NA",
-			Organization: "XnMatrix",
-			Address:      "f1yuz2twsllparyfqwslfiuxrc5wj4mfiflvnsw6a",
-			Website:      "@ Dr.Ann Shin on Filecoin Slack",
+			NotaryName:   "Holon Innovations",
+			Location:     "Oceania",
+			Organization: "Holon Global Investments PTY Ltd",
+			Address:      "f1ystxl2ootvpirpa7ebgwl7vlhwkbx2r4zjxwe5i",
 		},
 	}
 	for _, item := range list {
 		// record deletion processing
-		if item.Address == "f1sdzgaqmitbvgktkklpuaxohg6nuhce5eyvwxhaa" {
+		switch item.Address {
+		case "f1sdzgaqmitbvgktkklpuaxohg6nuhce5eyvwxhaa", "f2lda563hy3q2oxnoo265elynct3u2ft27ae7ptfy":
 			item.DeletedAt = &gorm.DeletedAt{
 				Valid: true,
 				Time:  time.Now(),
